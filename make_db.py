8d3fa964a24d0f8ead61e3fa2a72ddcb31eebce4
Holden Ranz <ranz.holden@gmail.com>
2015-04-29 17:38:08 -0400

    added sensitive files, checking .gitignore

--- make_db.py
+++ make_db.py
@@ -2,4 +2,4 @@ import sqlite3
 
 with sqlite3.connect("weather_users.db") as connection:
     c = connection.cursor()
-    c.execute("CREATE TABLE user_info(username TEXT, password TEXT, zipcode INT, phone_number TEXT)")
+    c.execute("CREATE TABLE user_info(phone_number TEXT, zipcode TEXT, verified INT, subscribed INT)")
